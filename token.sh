3557ed86bd7439ae2fbd8edf60589bbcf232badc
Noé Samaille <noe.samaille@protonmail.com>
2021-12-05 22:50:31 +0100

    Nextcloud deployment

--- token.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo $RANDOM > /dev/null; TOKEN=$(echo $RANDOM | md5sum | head -c 20; echo) && echo "{ \"token\": \"${TOKEN}\" }"
--- variables.tf
+++ variables.tf
@@ -6,6 +6,33 @@ variable "domain" {
   type        = string
   description = "Base domain name of your self-hosted services."
 }
+variable "smtp_from" {
+  type        = string
+  description = "Email to use for sending emails (invitations, etc)."
+}
+variable "smtp_host" {
+  type        = string
+  description = "SMTP server."
+  default     = "smtp.gmail.com"
+}
+variable "smtp_port" {
+  type        = number
+  description = "SMTP port."
+  default     = 587
+}
+variable "smtp_ssl" {
+  type        = bool
+  description = "Flag indicating SSL should be enabled for SMTP."
+  default     = true
+}
+variable "smtp_username" {
+  type        = string
+  description = "SMTP username."
+}
+variable "smtp_password" {
+  type        = string
+  description = "SMTP password."
+}
 
 #####################
 # MetalLB
@@ -34,55 +61,23 @@ variable "bitwarden_host_path" {
   type        = string
   description = "Local host path in with to store bitwarden persistent data."
 }
-
 variable "bitwarden_signups_allowed" {
   type        = bool
   description = "Flag indicating that signups are to we allowed in bitwarden."
   default     = false
 }
-
 variable "bitwarden_invitations_allowed" {
   type        = bool
   description = "Flag indicating that invitations are to we allowed in bitwarden."
   default     = true
 }
-
+variable "bitwarden_admin_token" {
+  type        = string
+  description = "Bitwarden admin access token."
+}
 variable "bitwarden_server_admin_email" {
   type        = string
   description = "Bitwarden admin email."
-}
-
-variable "bitwarden_smtp_host" {
-  type        = string
-  description = "SMTP server."
-  default     = "smtp.gmail.com"
-}
-
-variable "bitwarden_smtp_from" {
-  type        = string
-  description = "Email to use for sending emails (invitations, etc)."
-}
-
-variable "bitwarden_smtp_port" {
-  type        = number
-  description = "SMTP port."
-  default     = 587
-}
-
-variable "bitwarden_smtp_ssl" {
-  type        = bool
-  description = "Flag indicating SSL should be enabled for SMTP."
-  default     = true
-}
-
-variable "bitwarden_smtp_username" {
-  type        = string
-  description = "SMTP username."
-}
-
-variable "bitwarden_smtp_password" {
-  type        = string
-  description = "SMTP password."
 }
 
 #####################
@@ -92,4 +87,27 @@ variable "bitwarden_smtp_password" {
 variable "node_red_host_path" {
   type        = string
   description = "Local host path in with to store node-red persistent data."
+}
+
+#####################
+# Nextcloud
+#####################
+
+variable "nextcloud_host_path" {
+  type        = string
+  description = "Local host path in with to store nextcloud persistent data."
+}
+variable "nextcloud_admin_password" {
+  type        = string
+  description = "Admin password to access Nextcloud."
+}
+variable "nextcloud_mail_enabled" {
+  type        = bool
+  description = "Flag indicating that emails are to we enabled Nextcloud."
+  default   = true
+}
+variable "nextcloud_mail_domain" {
+  type        = string
+  description = "Email domain to use for Nextcloud."
+  default     = "gmail.com"
 }
